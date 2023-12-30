afd4b798ec2b877347aa0a2c387cbc8676468317
Konstantin Müller <muellerkonsti@web.de>
2023-12-30 10:43:52 +0100

    excercise 2.1 - icp alignment

--- Lesson_6_Utilizing_Scan_Matching/Exercise-ICP-Alignment/sm1-main.cpp
+++ Lesson_6_Utilizing_Scan_Matching/Exercise-ICP-Alignment/sm1-main.cpp
@@ -223,7 +223,7 @@ int main(){
 
 		if( matching != Off){
 			if( matching == Icp)
-				transform = ICP(mapCloud, cloudFiltered, pose, 3); //TODO: change the number of iterations to positive number
+				transform = ICP(mapCloud, cloudFiltered, pose, 2); //TODO: change the number of iterations to positive number
   			pose = getPose(transform);
 			if( !tester.Displacement(pose) ){
 				if(matching == Icp)
